2b88758f5f8b0884d0199a10b4c70f76f9481498
Dan Snow <danjsnow@outlook.com>
2026-05-31 10:59:45 +0000

    :necktie: Just use MO layer for numbers
    Have left the sticky switch logic while I test it out.

--- keyboards/zsa/voyager/keymaps/dan-snw/keymap.c
+++ keyboards/zsa/voyager/keymaps/dan-snw/keymap.c
@@ -18,7 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ENTER,       KC_Q,           KC_W,                 KC_F,               KC_P,               KC_B,                           KC_J,           LT(3, KC_L),        KC_U,        KC_Y,                 KC_QUOTE,        KC_NO,
     OSM(MOD_RCTL),  KC_A,           KC_R,                 KC_S,               HYPR_T(KC_T),       KC_G,                           KC_M,           HYPR_T(KC_N),       KC_E,        KC_I,                 KC_O,            KC_NO,
     TT(3),          KC_Z,           MT(MOD_LGUI, KC_X),   MT(MOD_LALT, KC_C), MT(MOD_RCTL, KC_D), KC_V,                           KC_K,           MT(MOD_RCTL, KC_H), OSL(1),      MT(MOD_LGUI, KC_DOT), KC_SLASH,        KC_NO,
-                                                                              OSL2_STICKY,        OSM(MOD_LSFT),                  OSM(MOD_LSFT),  KC_SPACE
+                                                                              MO(2),              OSM(MOD_LSFT),                  OSM(MOD_LSFT),  KC_SPACE
+
   ),
   [1] = LAYOUT_voyager(
     TRA,            TRA,            TRA,                  TRA,                TRA,                TRA,                            TRA,            TRA,                TRA,         TRA,                  TRA,             TRA,  
